aab37e10b535f4a3f213efff1974e24d83d24fdf
ndayingiye <ndayiringiyedavid120@gmail.com>
2025-03-31 13:20:39 +0200

    feat:/bild user schema and database connected successfully

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import { connectbd } from "./config/db.js";
 
 dotenv.config()
 
@@ -8,6 +9,7 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () =>{
+app.listen(5000, async () =>{
+    await connectbd();
     console.log(`server is running on port ${PORT}`)
 });
